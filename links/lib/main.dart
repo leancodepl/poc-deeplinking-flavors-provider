8af2cb140860308b1117050b5a2303685b8134b0
PiotrRogulski <piotr.rogulski@leancode.pl>
2022-11-21 15:23:54 +0100

    Enable link logging

--- links/lib/main.dart
+++ links/lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:links/home_page.dart';
 import 'package:links/links/handlers/logging_handler.dart';
 import 'package:links/links/link_listener.dart';
+import 'package:logging/logging.dart';
 
-void main() {
+void main() async {
+  await _setupLogger();
+
   runApp(const MyApp());
 }
 
@@ -27,4 +31,28 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+Future<void> _setupLogger() async {
+  if (kDebugMode) {
+    Logger.root.level = Level.ALL;
+    // ignore: avoid_print
+    Logger.root.onRecord.listen(print);
+  } else {
+    // Add custom logging in release mode
+  }
+
+  final flutterErrorLogger = Logger('FlutterError');
+
+  FlutterError.onError = (details) {
+    if (kDebugMode) {
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      flutterErrorLogger.warning(
+        details.summary.name,
+        details.exception,
+        details.stack,
+      );
+    }
+  };
 }
