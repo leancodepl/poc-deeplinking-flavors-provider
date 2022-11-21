f8ef1fe62b0d35b1a4ce4c9ed8c0f2c5f26e6bf0
PiotrRogulski <piotr.rogulski@leancode.pl>
2022-11-21 16:35:30 +0100

    Add explanation on the home page

--- links/lib/home_page.dart
+++ links/lib/home_page.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void _share() {
+    // TODO: Share the app
+    throw UnimplementedError();
   }
 
   @override
@@ -22,24 +14,73 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Links Demo'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(24),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Text(
+              'Links schemas that can be opened in this app:',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'https://links.example.com',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'links://example.com',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            const SizedBox(height: 32),
+            Text(
+              'Available routes:',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'links://example.com/query?...',
+              style: Theme.of(context).textTheme.bodyLarge,
             ),
             Text(
-              '$_counter',
+              'Shows the query parameters of the link',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'links://example.com/path/...',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            Text(
+              'Shows the path segments of the link',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            const SizedBox(height: 32),
+            Text(
+              'All links can be blocked by setting the query parameter "blocked" to "true".',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            const SizedBox(height: 32),
+            Text(
+              'Share a deeplink to this app:',
               style: Theme.of(context).textTheme.headline4,
+            ),
+            const SizedBox(height: 4),
+            ElevatedButton(
+              onPressed: _share,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Icon(Icons.share),
+                  SizedBox(width: 8),
+                  Text('Share'),
+                ],
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
--- links/lib/links/handlers/link_navigator.dart
+++ links/lib/links/handlers/link_navigator.dart
@@ -3,31 +3,43 @@ import 'package:links/features/link_path/link_path_page.dart';
 import 'package:links/features/link_query/link_query_page.dart';
 import 'package:links/home_page.dart';
 import 'package:links/links/link_handler.dart';
+import 'package:logging/logging.dart';
 
 class LinkNavigator extends LinkHandler {
-  const LinkNavigator({
+  LinkNavigator({
     required this.context,
   });
 
   final BuildContext context;
 
+  final logger = Logger('LinkNavigator');
+
   @override
   bool handle(Uri link) {
     final route = _routeForLink(link);
-    Navigator.of(context).push<dynamic>(route);
+    if (route != null) {
+      Navigator.of(context).push<dynamic>(route);
+    } else {
+      logger.warning('No route found for link: $link');
+    }
+
     return true;
   }
 
-  Route<dynamic> _routeForLink(Uri link) {
-    switch (link.path) {
-      case '/':
-        return MaterialPageRoute<void>(
-          builder: (context) => const HomePage(),
-        );
-      case '/query':
+  Route<dynamic>? _routeForLink(Uri link) {
+    if (link.pathSegments.isEmpty) {
+      return MaterialPageRoute<void>(
+        builder: (context) => const HomePage(),
+      );
+    }
+
+    switch (link.pathSegments.first) {
+      case 'query':
         return LinkQueryPage(queryParameters: link.queryParameters);
-      default:
+      case 'path':
         return LinkPathPage(pathSegments: link.pathSegments);
+      default:
+        return null;
     }
   }
 }
